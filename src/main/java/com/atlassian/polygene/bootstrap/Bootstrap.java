823bbb64650e23157a1505b27327e94209700e90
Dominic Jones <dojones@atlassian.com>
2017-06-02 07:46:00 +1000

    Factory implementation

--- src/main/java/com/atlassian/polygene/bootstrap/Bootstrap.java
+++ src/main/java/com/atlassian/polygene/bootstrap/Bootstrap.java
@@ -1,8 +1,10 @@
 package com.atlassian.polygene.bootstrap;
 
-import com.atlassian.polygene.hello.polygene2.HelloWorldComposite;
+import com.atlassian.polygene.hello.polygene6.HelloWorldComposite;
 import org.qi4j.api.activation.ActivationException;
+import org.qi4j.api.service.ServiceReference;
 import org.qi4j.api.structure.Application;
+import org.qi4j.api.structure.Module;
 import org.qi4j.bootstrap.ApplicationAssembly;
 import org.qi4j.bootstrap.AssemblyException;
 import org.qi4j.bootstrap.Energy4Java;
@@ -21,12 +23,19 @@ public class Bootstrap {
 
             domain.uses(persistence);
             ModuleAssembly helloWorld = domain.module("hello-world");
-            helloWorld.entities(HelloWorldComposite.class);
+            helloWorld.transients(HelloWorldComposite.class);
+            helloWorld.services(HelloWorldFactory.class);
 
             return assembly;
         });
 
         installShutdownHook(application);
+
+        Module domain = application.findModule("domain", "hello-world");
+        ServiceReference<HelloWorldFactory> service = domain.findService(HelloWorldFactory.class);
+        HelloWorldFactory helloWorldFactory = service.get();
+        HelloWorldComposite helloWorld = helloWorldFactory.create();
+        System.out.println(helloWorld.say());
 
         application.activate();
     }
---
+++ src/main/java/com/atlassian/polygene/bootstrap/HelloWorldFactory.java
@@ -0,0 +1,30 @@
+package com.atlassian.polygene.bootstrap;
+
+import com.atlassian.polygene.hello.polygene6.HelloWorldComposite;
+import com.atlassian.polygene.hello.polygene6.HelloWorldState;
+import org.qi4j.api.composite.TransientBuilder;
+import org.qi4j.api.composite.TransientBuilderFactory;
+import org.qi4j.api.injection.scope.Structure;
+import org.qi4j.api.mixin.Mixins;
+
+@Mixins(HelloWorldFactory.HelloWorldFactoryMixin.class)
+public interface HelloWorldFactory {
+
+    HelloWorldComposite create();
+
+    class HelloWorldFactoryMixin implements HelloWorldFactory {
+
+        @Structure
+        TransientBuilderFactory tbf;
+
+        public HelloWorldComposite create() {
+            TransientBuilder<HelloWorldComposite> builder = tbf.newTransientBuilder(HelloWorldComposite.class);
+
+            HelloWorldState helloWorldState = builder.prototypeFor(HelloWorldState.class);
+            helloWorldState.name().set("Dom");
+            helloWorldState.phrase().set("stop");
+
+            return builder.newInstance();
+        }
+    }
+}
